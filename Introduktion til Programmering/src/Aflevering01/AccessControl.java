d98a49eb0cfb68014d7dce33f6ee9098ec6e764e
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2022-10-05 20:09:37 +0200

    +

--- Introduktion til Programmering/src/Aflevering01/AccessControl.java
+++ Introduktion til Programmering/src/Aflevering01/AccessControl.java
@@ -9,42 +9,87 @@ public class AccessControl {
 		String userName = "";
 		String userPassword = "";
 
+		boolean login = false;
 		boolean userCheck = false;
-		Boolean passwordCheck = false;
-		Scanner input = new Scanner(System.in);
+		boolean passwordCheck = false;
 		
 		
+		while(true){
+			Scanner input = new Scanner(System.in);
 
-		while(!userCheck){
-			System.out.println("Please enter a non-empty username: ");
-			userName = input.nextLine();
-			if(userName != ""){
-				userCheck = true;
-			}		
-		}
-		
-		while(!passwordCheck){
-			System.out.println("Please insert a password: ");
+			while(!userCheck){
+				System.out.println("Please enter a non-empty username: ");
+				userName = input.nextLine();
+				if(userName != ""){
+					userCheck = true;
+				}		
+			}
 			
-			userPassword = input.nextLine();
-
-			if(Password.checkPWD(userPassword)){
-
-				System.out.println("Please repeat the password: ");
-
+			while(!passwordCheck){
+				System.out.println("Please insert a password: ");
 				
-				if(input.nextLine().equals(userPassword)){
-					passwordCheck = true;
+				userPassword = input.nextLine();
+
+				if(Password.checkPWD(userPassword)){
+
+					System.out.println("Please repeat the password: ");
+
+					
+					if(input.nextLine().equals(userPassword)){
+						passwordCheck = true;
+					}
+					
+				}
+			}
+			System.out.println("*** User "+ userName + " is registered");
+			
+			
+
+			while(!login){
+				
+				System.out.print("To logon enter Username: ");
+				
+				if(input.nextLine().equals(userName)){
+					System.out.println("To logon enter Password: ");
+					if(input.nextLine().equals(userPassword)){
+						login = true;
+					}
+				}
+			}
+			
+			System.out.println("*** User " + userName + " is logged on");
+
+			System.out.println("You now have the following choices: \n 1 - Change password \n 2 - Log off \n 3 - Shut down");
+
+			String str = input.nextLine();
+
+			if(str.equals("1") || str.equals("2") || str.equals("3")){
+
+				switch(str){
+					case "1":
+						System.out.println("Indtast din nuværende password: ");
+						String inputPassword = input.nextLine();
+
+						if(inputPassword.equals(userPassword)){
+							
+						}
+						break;
+					case "2":
+
+						break;
+					case "3":
+
+						break;
+					default:
 				}
 				
+				
+				
 			}
+
+			input.close();
 		}
-		System.out.println("*** User "+ userName + " is registered");
-		
-		
-		
 
-
-		input.close();
+		
 	}
 }
