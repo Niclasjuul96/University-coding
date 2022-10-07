3bb8840ca65a9cd6d47ac48eabaaa97ba973cc4d
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2022-10-07 12:31:33 +0200

    Total breakThrough, Mangler Contains

--- Introduktion til Programmering/src/Aflevering01/TextAnalysis22.java
+++ Introduktion til Programmering/src/Aflevering01/TextAnalysis22.java
@@ -8,24 +8,29 @@ import java.util.Scanner;
 
 public class TextAnalysis22 {
 
+    String array1[];
+    int words = 0; 
 
-
-    public TextAnalysis22(String souceFileName , int i) throws FileNotFoundException {
+    public TextAnalysis22(String souceFileName , int maxNoOfWords ) throws FileNotFoundException {
 
         File text = new File(souceFileName);
+
+        array1 = new String[maxNoOfWords];
        
         Scanner sc1= new Scanner(text);
-        String data1 = "";
-        String array1[] = {};
-
     
-        while(sc1.hasNextLine()){
-            data1 = data1 + "\n"+ sc1.nextLine();       
+        while(sc1.hasNextLine()){  
+            String[] line = sc1.nextLine().split("[^a-zA-Z]+");   
+            System.out.println(line.length);
+            for(int i = 0; i < line.length; i++){
+
+                if(line[i] != null){
+                    this.array1[words + i] = line[i];
+                    words += 1;
+                }
+            }
+            line = null;
         }
-        array1 = data1.split("[^a-zA -Z]+");
-
-        System.out.println(array1.length);
-
         sc1.close();
     }
 
@@ -34,41 +39,43 @@ public class TextAnalysis22 {
 
         TextAnalysis22 ta22 = new TextAnalysis22("simpletext01.txt", 40);
 
-        int i = ta22.frequency("es");
-        //int i2 = ta22.wordcount();
-        
-        //System.out.println(i);
+        int n = ta22.frequency("es");
+        System.out.println(n);
+        n = ta22.frequency("ES");
+        System.out.println(n);
+        int i = ta22.wordcount();
+        System.out.println(i);
     }
 
 
 
-    public int wordcount(String str) throws FileNotFoundException
-    {
-
-       
+    public int wordcount() throws FileNotFoundException
+    {       
         // returns the number of words in the file
-
-        int i = 0;
-        return i;
+        return words;
     }
 
-    public int frequency(String string)
+    public int frequency(String word)
     {
-
-        
-
         // returns the number of occurrences
         // of " word " in the text (case - sensitive ).
 
-        int i = string.hashCode();
+        int count = 0;
 
-        return i;
+        for (int i = 0;i < array1.length;i++) {
+            if(array1[i] != null){
+                if(array1[i].equals(word)){
+                count++;
+                }
+            }
+            
+        }
+        return count;
     }
 
     public boolean contains(String word1, String word2)
     {
         
-
         
         // returns true if word1 is directly followed by word2 somewhere
         // in the text (see above , not case - sensitive ).
