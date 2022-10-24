c704f0350162df10425707c4b6f04b11f0c10b7f
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2022-10-24 20:01:52 +0200

    +

--- Introduktion til Programmering/src/Uge03/Exercise1.java
+++ Introduktion til Programmering/src/Uge03/Exercise1.java
@@ -1,7 +1,21 @@
 package Uge03;
+import java.util.Scanner;
 
 public class Exercise1 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int tal1;
+        int tal2;
+
+        String svar = input.nextLine();
         
+        tal1 = Character.getNumericValue(svar.charAt(0));
+        tal2 = Character.getNumericValue(svar.charAt(2));
+
+        input.close();
+        
+        int sum = tal1 + tal2;
+        System.out.println(sum);
     }
 }
