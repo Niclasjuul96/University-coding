ac26fd6bbb3a8582812abdbaaf49ccd539411f8b
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2022-11-07 16:19:28 +0100

    +

---
+++ Introduktion til Programmering/src/Aflevering02/Fraction.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+    private int a,b;
+}
+
+
+public static Fraction(int numerator, denominator) {
+    Fraction f = (numerator,denominator);
+
+    return f;
+}
+
+public static plus(Fraction f){
+    
+
+    System.out.println(fr);
+}
+
+public static minus(Fraction f){
+
+}
+
+public static times(Fraction f){
+
+}
+
+public static divideBy(Fraction f){
+
+}
+
+public static reciprocal(){
+
+}
