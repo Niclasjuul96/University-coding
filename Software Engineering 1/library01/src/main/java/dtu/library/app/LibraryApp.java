bf4b9a09a3929bb76c519c8f63943e3d2b3b729c
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2023-01-31 14:09:02 +0100

    +

---
+++ Software Engineering 1/library01/src/main/java/dtu/library/app/LibraryApp.java
@@ -0,0 +1,23 @@
+package dtu.library.app;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LibraryApp {
+
+	public void addBook(Book book) throws OperationNotAllowedException {
+	}
+
+	public boolean containsBookWithSignature(String signature) {
+		return false;
+	}
+
+	public boolean adminLoggedIn(){
+
+
+		return true;
+	}
+
+	
+}
