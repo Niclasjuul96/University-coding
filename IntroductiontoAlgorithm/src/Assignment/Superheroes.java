8223c311e2896d92e4e651643def3704466681af
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2023-02-28 11:11:15 +0100

    +

--- IntroductiontoAlgorithm/src/Assignment/Superheroes.java
+++ IntroductiontoAlgorithm/src/Assignment/Superheroes.java
@@ -1,16 +1,33 @@
 package Assignment;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Superheroes {
     public static void main(String[] args) {
         
-        ArrayList[][] T = new ArrayList[10][10];
-        ArrayList[][] S = new ArrayList[10][10];
+        ArrayList<Integer> T = new ArrayList<Integer>(6);
+        ArrayList<Integer> S = new ArrayList<Integer>(10);
 
+        Random rand = new Random();
 
-        
+        for(int i = 0; i < 10; i++){
+            S.add(rand.nextInt(100));
+        }
 
+        for(int i = 0; i < S.size(); i++){
+            System.out.println(S.get(i));
+        }
+
+        for(int i = 0; i < 6; i++){
+            T.add(S.get(rand.nextInt(10)));
+        }
+
+        System.out.println("done Random generation");
+
+        for(int i = 0; i < T.size(); i++){
+            System.out.println(T.get(i));
+        }
 
     }
 }
