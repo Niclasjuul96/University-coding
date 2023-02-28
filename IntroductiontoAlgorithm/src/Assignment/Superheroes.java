08842e233a3dca1d911abad6fede96ab87dd2d78
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2023-02-28 10:32:58 +0100

    +

---
+++ IntroductiontoAlgorithm/src/Assignment/Superheroes.java
@@ -0,0 +1,16 @@
+package Assignment;
+
+import java.util.ArrayList;
+
+public class Superheroes {
+    public static void main(String[] args) {
+        
+        ArrayList[][] T = new ArrayList[10][10];
+        ArrayList[][] S = new ArrayList[10][10];
+
+
+        
+
+
+    }
+}
--- IntroductiontoAlgorithm/src/week0/Intro.java
+++ IntroductiontoAlgorithm/src/week0/Intro.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class intro {
+public class Intro {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Read input from System.in
         int input = scanner.nextInt();
