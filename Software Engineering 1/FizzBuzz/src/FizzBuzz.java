52c0c5283e99ca6ce1e0f887b1dbce21a1be0c6d
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2023-03-13 12:33:55 +0100

    +

---
+++ Software Engineering 1/FizzBuzz/src/FizzBuzz.java
@@ -0,0 +1,26 @@
+
+public class FizzBuzz {
+    String str;
+    public static void main(String[] args) throws Exception {
+        for(int i = 0; i < 100; i++) {
+            System.out.println(Calculate(i));
+        }
+
+    }
+
+
+    public static String Calculate(int i){
+        if (i % 3 == 0 && i % 5 == 0){
+            String str = "FizzBuzz";
+            return str;
+        } else if (i % 5 == 0){
+            String str = "Buzz";
+            return str;
+        }else if(i % 3 == 0){
+            String str = "Fizz";
+            return str;
+        }else{
+            return i + "";
+        }
+    } 
+}
