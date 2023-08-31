7a4d7c8d675a5a31d09a011487b8847a6906ba69
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2023-08-31 22:03:03 +0200

    +++

--- Functional-Programming/MyFSharpApp/Program.fs
+++ Functional-Programming/MyFSharpApp/Program.fs
@@ -1,6 +1,3 @@
-﻿// Define a new function to print a name.
-let printGreeting name =
-    printfn $"Hello {name} from F#!"
-
-// Call your new function!
-printGreeting "Ana"
+﻿let rec fact n =
+if n=0 then 1
+else n * fact(n-1);;
